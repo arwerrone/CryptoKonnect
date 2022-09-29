3454ebae8db683845ec88051bc02c72d0a30edc7
thhuynh7 <thhuynh7@myseneca.ca>
2022-09-29 13:14:12 -0400

    footer updated

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -5,29 +5,29 @@ export default class Footer extends Component {
   render() {
     return (
       <footer>
-        <div class="content">
-          <div class="top">
-            <div class="logo-details">
-              <span class="logo_name">Crypto Konnect</span>
+        <div className="content">
+          <div className="top">
+            <div className="logo-details">
+              <span className="logo_name">Crypto Konnect</span>
             </div>
-            <div class="media-icons">
+            <div className="media-icons">
               <a href="/">
-                <i class="fab fa-facebook-f"></i>
+                <i className="fab fa-facebook-f"></i>
               </a>
               <a href="https://github.com/arwerrone/CryptoKonnect">
-                <i class="fab fa-github"></i>
+                <i className="fab fa-github"></i>
               </a>
               <a href="/">
-                <i class="fab fa-instagram"></i>
+                <i className="fab fa-instagram"></i>
               </a>
               <a href="/">
-                <i class="fab fa-linkedin-in"></i>
+                <i className="fab fa-linkedin-in"></i>
               </a>
             </div>
           </div>
-          <div class="link-boxes">
-            <ul class="box">
-              <li class="link_name">Crypto Konnect</li>
+          <div className="link-boxes">
+            <ul className="box">
+              <li className="link_name">Crypto Konnect</li>
               <li>
                 <a href="/">About Us</a>
               </li>
@@ -38,8 +38,8 @@ export default class Footer extends Component {
                 <a href="/">Upcoming Events</a>
               </li>
             </ul>
-            <ul class="box">
-              <li class="link_name">Quick Links</li>
+            <ul className="box">
+              <li className="link_name">Quick Links</li>
               <li>
                 <a href="#home">Home</a>
               </li>
@@ -51,8 +51,8 @@ export default class Footer extends Component {
               </li>
             </ul>
 
-            <ul class="box">
-              <li class="link_name">Legal</li>
+            <ul className="box">
+              <li className="link_name">Legal</li>
               <li>
                 <a href="/">Terms & Conditions</a>
               </li>
@@ -62,9 +62,9 @@ export default class Footer extends Component {
             </ul>
           </div>
         </div>
-        <div class="bottom-details">
-          <div class="bottom_text">
-            <span class="copyright_text">
+        <div className="bottom-details">
+          <div className="bottom_text">
+            <span className="copyright_text">
               Copyright © 2022
               <span> </span><a href="/">Crypto Konnect</a>
               All rights reserved
