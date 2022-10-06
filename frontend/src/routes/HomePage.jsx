1e0bca08839920faba3ccd93c7023998dc4d7c77
thhuynh7 <thhuynh7@myseneca.ca>
2022-10-06 07:16:41 -0400

    add top trending to homepage

--- frontend/src/routes/HomePage.jsx
+++ frontend/src/routes/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import HomeHero from '../components/home/HomeHero';
 import HomeList from '../components/home/HomeList';
 
 const HomePage = props => {
   return (
     <div>
+      <HomeHero />
       <HomeList cryptos={props.cryptos} />
     </div>
   );
