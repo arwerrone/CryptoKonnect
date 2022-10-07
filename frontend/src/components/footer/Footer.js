e71c7cbc4b839479241d5775fa15a904f03342fb
thhuynh7 <thhuynh7@myseneca.ca>
2022-10-06 21:24:36 -0400

    update footer

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -4,13 +4,11 @@ import './Footer.css';
 export default class Footer extends Component {
   render() {
     return (
-      <footer className='bg-gray-300'>
-        <div className="content">
-          <div className="top">
-            <div className="logo-details">
-              <span className="logo_name">Crypto Konnect</span>
-            </div>
-            <div className="media-icons">
+      <footer className="bg-gray-300">
+        <div className="container">
+          <div className="social">
+            <span className="text-4xl">Crypto Konnect</span>
+            <div className="icon">
               <a href="/">
                 <i className="fab fa-facebook-f"></i>
               </a>
@@ -25,9 +23,9 @@ export default class Footer extends Component {
               </a>
             </div>
           </div>
-          <div className="link-boxes">
-            <ul className="box">
-              <li className="link_name">Crypto Konnect</li>
+          <div className="link">
+            <ul className="col">
+              <li>Crypto Konnect</li>
               <li>
                 <a href="/">About Us</a>
               </li>
@@ -38,37 +36,33 @@ export default class Footer extends Component {
                 <a href="/">Upcoming Events</a>
               </li>
             </ul>
-            <ul className="box">
-              <li className="link_name">Quick Links</li>
+            <ul className="col">
+              <li>Quick Links</li>
               <li>
-                <a href="#home">Home</a>
+                <a href="/">Home</a>
               </li>
               <li>
-                <a href="#dashboard">Dashboard</a>
+                <a href="/dashboard">Dashboard</a>
               </li>
               <li>
                 <a href="/">Contact Us</a>
               </li>
             </ul>
 
-            <ul className="box">
-              <li className="link_name">Legal</li>
+            <ul className="col">
+              <li>Legal</li>
               <li>
                 <a href="/">Terms & Conditions</a>
               </li>
               <li>
                 <a href="/">Privacy Policy</a>
               </li>
+              <li>
+                <a href="/">License Agreement</a>
+              </li>
+              <li></li>
             </ul>
-          </div>
-        </div>
-        <div className="bottom-details">
-          <div className="bottom_text">
-            <span className="copyright_text">
-              Copyright © 2022
-              <span> </span><a href="/">Crypto Konnect</a>
-              All rights reserved
-            </span>
+            <p>Copyright 2022 Crypto Konnect</p>
           </div>
         </div>
       </footer>
