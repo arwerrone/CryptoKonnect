d67875ea99c2d8eca9cfda18f20d77030977b815
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2022-10-13 15:01:49 -0400

    Import token finished

--- frontend/src/components/wallet/WalletComp.js
+++ frontend/src/components/wallet/WalletComp.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row, Col, Card, Button, Form, Stack,  } from "react-bootstrap";
+import { Container, Row, Col, Card, Button, Form, Stack, Alert, } from "react-bootstrap";
 import { injected } from "./connectors";
 import {useWeb3React} from '@web3-react/core'
 import { ethers } from "ethers";
@@ -47,33 +47,55 @@ export default function WalletComp() {
   const [mainnet,setMainnet]= useState();
   const [selectedToken, setSelectedToken] = useState(TokenListMainnet[0]);
   const [balance] = useBalance(selectedToken.address, selectedToken.decimals)
+//////////////
+  const [error2, setError2] = useState();
+  const [tokenImported, setTokenImported] = useState(false);
+  const addTokenToWallet = async () => {
+    try {
+      await window.ethereum.request({
+        method: 'wallet_watchAsset',
+        params: {
+          type: 'ERC20',
+          options: {
+            address: selectedToken.address,
+            symbol: selectedToken.symbol,
+            decimals: selectedToken.decimals,
+          },
+        },
+      })
+      setTokenImported(true);
+    }catch(err){
+      setError2(err.message);
+    }
+  }
+
 ///////////////////////////////////////////////
   const [error, setError] = useState();
   const [txs, setTxs] = useState([]);
 
 
-const startPayment = async ({ setError, setTxs, ether, addr }) => {
-  try {
-    if (!window.ethereum){
-      throw new Error("No crypto wallet found. Please install it.");
-    }
+  const startPayment = async ({ setError, setTxs, ether, addr }) => {
+    try {
+      if (!window.ethereum){
+        throw new Error("No crypto wallet found. Please install it.");
+      }
 
-    await window.ethereum.send("eth_requestAccounts");
-    await activate(injected)
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    ethers.utils.getAddress(addr);
-    const tx = await signer.sendTransaction({
-      to: addr,
-      value: ethers.utils.parseEther(ether)
-    });
-    console.log({ ether, addr });
-    console.log("tx", tx);
-    setTxs([tx]);
-  } catch (err) {
-    setError(err.message);
-  }
-};
+      await window.ethereum.send("eth_requestAccounts");
+      await activate(injected)
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      ethers.utils.getAddress(addr);
+      const tx = await signer.sendTransaction({
+        to: addr,
+        value: ethers.utils.parseEther(ether)
+      });
+      console.log({ ether, addr });
+      console.log("tx", tx);
+      setTxs([tx]);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -131,15 +153,15 @@ const startPayment = async ({ setError, setTxs, ether, addr }) => {
                   <Stack gap={3}>
 
                   {active ? <Card.Title>{account}</Card.Title> : <Card.Title>NOT CONNECTED</Card.Title>}
-
-                  <Form.Select value={mainnet} onChange={ e=> {setMainnet(e.target.value)}}>
+TokenListMainnet[0]
+                  <Form.Select value={mainnet} onChange={ e=> {setMainnet(e.target.value); setSelectedToken(TokenListMainnet[0]); setTokenImported(false);}}>
                     <option className="text-center">--Select Mainnet--</option>
                     <option className="text-center" value="1">Ethereum Mainnet</option>
                     <option className="text-center" value="2">Polygon Mainnet</option>
                   </Form.Select>
 
                   {mainnet === '1' &&
-                      <><Form.Select onChange={e => { setSelectedToken(TokenListMainnet[e.target.value]); } }>
+                      <><Form.Select onChange={e => { setSelectedToken(TokenListMainnet[e.target.value]); setTokenImported(false);} }>
                       {TokenListMainnet.map((token, index) => (
                         <option value={index} key={token.address}>{token.name}</option>
                       ))}
@@ -151,7 +173,7 @@ const startPayment = async ({ setError, setTxs, ether, addr }) => {
 
                   {mainnet === '2' &&
 
-                    <><Form.Select onChange={e => { setSelectedToken(TokenListPolygon[e.target.value]); } }>
+                    <><Form.Select onChange={e => { setSelectedToken(TokenListPolygon[e.target.value]); setTokenImported(false);} }>
                       {TokenListPolygon.map((token, index) => (
                         <option value={index} key={token.address}>{token.name}</option>
                       ))}
@@ -161,9 +183,14 @@ const startPayment = async ({ setError, setTxs, ether, addr }) => {
                     </>
                   }
 
+                  {active && mainnet ? <Button onClick={addTokenToWallet} variant="outline-secondary">Add {selectedToken.name} to Wallet</Button> : null}
+
                   {active ? <Button onClick={disconnect} variant="outline-primary">Disconnect Wallet</Button> :
                   <Button onClick={connect} variant="outline-primary">Connect Wallet</Button> }
                   
+                  <ErrorMessage message={error2} />
+                  {tokenImported ? <Alert key="info" variant="info">{selectedToken.name} added to your wallet!</Alert> : null}
+
                   </Stack>
 
                 </Card.Body>
