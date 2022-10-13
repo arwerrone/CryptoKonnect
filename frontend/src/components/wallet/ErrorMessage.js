4c5566dd48f860a9b057c3ddcc87ee9ef2b3f5e8
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2022-10-13 03:06:42 -0400

    transtaction Error logger

--- frontend/src/components/wallet/ErrorMessage.js
+++ frontend/src/components/wallet/ErrorMessage.js
@@ -6,11 +6,9 @@ export default function ErrorMessage({ message }) {
     return (
       <div className="alert alert-error mt-5">
         <div className="flex-1">
-
             <Alert key="danger" variant="danger">
             <label>{message}</label>
             </Alert>
-
         </div>
       </div>
     );
