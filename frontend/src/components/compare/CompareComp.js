15fefca6164f7991bbbd47bef942544196b3b6a2
John <jyzhang7@myseneca.ca>
2022-10-20 14:56:30 -0400

    compare feature updated

--- frontend/src/components/compare/CompareComp.js
+++ frontend/src/components/compare/CompareComp.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import React, { Fragment, useEffect, useState } from 'react';
 import { Card } from 'react-bootstrap';
 import { ReactDOM } from 'react-dom';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 
 
@@ -131,8 +132,12 @@ const CompareComp = props => {
     const [state, setState] = useState([])
     const [state2, setState2] = useState([])
 
+    let coinListUrl = `https://api.coingecko.com/api/v3/coins/list`;
+    let singleCoinUrl = `https://api.coingecko.com/api/v3/coins/`;
+
     useEffect(() => {
-        axios.get(`https://api.coingecko.com/api/v3/coins/list`).then((res) => {
+        console.log("coin list grabbing");
+        axios.get(coinListUrl).then((res) => {
             setCoin(res.data)
         }).catch((error) => {
             console.log(error)
@@ -141,24 +146,32 @@ const CompareComp = props => {
 
     //function SetWindow1(selectedcoin) {
     const SetWindow1 = (selectedcoin) => {
-        console.log("Calling API")
-        useEffect(() => {
-            axios.get(`https://api.coingecko.com/api/v3/coins/${selectedcoin}`).then((res) => {
-                console.log("In API");
-                setCoin1(res.data);
-            }).catch((error) => {
-                console.log(error)
-            })
-        },)
-        console.log("In API");
+        console.log("Calling API");
+        axios.get(singleCoinUrl + selectedcoin).then((res) => {
+            console.log("In API");
+            setCoin1(res.data);
+
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+
+    const SetWindow2 = (selectedcoin) => {
+        console.log("Calling API");
+        axios.get(singleCoinUrl + selectedcoin).then((res) => {
+            console.log("In API");
+            setCoin2(res.data);
+
+        }).catch((error) => {
+            console.log(error)
+        })
     }
 
     //function SetWindow2(selectedcoin){
-    function SetWindow2(selectedcoin) {
+    function SetWindow22(selectedcoin) {
         useEffect(() => {
             axios.get(`https://api.coingecko.com/api/v3/coins/${selectedcoin}`).then((res) => {
                 setCoin2(res.data)
-                //console.log(res.data)
             }).catch((error) => {
                 console.log(error)
             })
@@ -184,8 +197,25 @@ const CompareComp = props => {
                             }
                         </select>
                         {renderSelectedCoin1(state.selectedCoin1)}
-
-
+                        {console.log("printing data")}    
+                        <thead>
+                            <tr className="border-b border-t">
+                                <th>Name</th>
+                                <th>Price(CAD)</th>
+                                <th>24h</th>
+                                <th>24h Volume</th>
+                                <th>Market</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>{coin1.name}</td>
+                                <td>${coin1.market_data.current_price.cad}</td>
+                                <td>{coin1.market_data.price_change_percentage_24h}%</td>
+                                <td>${coin1.market_data.market_cap.cad}</td>
+                                <td>${coin1.market_data.total_volume.cad}</td>
+                            </tr>
+                        </tbody>
                     </div>
 
                     <div className="col-sm-6">
@@ -198,45 +228,70 @@ const CompareComp = props => {
                             ))
                             }
                         </select>
-                        {renderSelectedCoin1(state2.selectedCoin2)}
+                        {renderSelectedCoin2(state2.selectedCoin2)}
+                    
+                        <thead>
+                            <tr className="border-b border-t">
+                                <th>Name</th>
+                                <th>Price(CAD)</th>
+                                <th>24h</th>
+                                <th>24h Volume</th>
+                                <th>Market</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>{coin2.name}</td>
+                                <td>${coin2.market_data.current_price.cad}</td>
+                                <td>{coin2.market_data.price_change_percentage_24h}%</td>
+                                <td>${coin2.market_data.market_cap.cad}</td>
+                                <td>${coin2.market_data.total_volume.cad}</td>
+                            </tr>
+                        </tbody>
                     </div>
                 </div>
-
             </div>
         </>
     )
 
 
-
-    function renderSelectedCoin1(selectedcoin) {
-
-        if (selectedcoin !== "0" && selectedcoin) {
-
-            console.log(selectedcoin)
+    function renderSelectedCoin1(selectedcoin1) {
+        if (selectedcoin1 !== "0" && selectedcoin1) {
+            console.log("1 this is " + selectedcoin1)
             //console.log(props.crypto.map)
 
-            if (prev1 !== selectedcoin) {
-                prev1 = selectedcoin
+            if (prev1 !== selectedcoin1) {
+                prev1 = selectedcoin1
+                console.log("coin copied");
                 rerender1 = true
             }
 
             if (rerender1) {
                 rerender1 = false
                 console.log("setting window")
-                SetWindow1(selectedcoin)
-
+                SetWindow1(selectedcoin1)
             }
-
-
-
         }
-
     }
 
+    function renderSelectedCoin2(selectedcoin2) {
+        if (selectedcoin2 !== "0" && selectedcoin2) {
+            console.log("2 this is " + selectedcoin2)
+            //console.log(props.crypto.map)
 
+            if (prev2 !== selectedcoin2) {
+                prev2 = selectedcoin2
+                console.log("coin copied");
+                rerender2 = true
+            }
+
+            if (rerender2) {
+                rerender2 = false
+                console.log("setting window")
+                SetWindow2(selectedcoin2)
+            }
+        }
+    }
 }
-
-
-
 
 export default CompareComp;
